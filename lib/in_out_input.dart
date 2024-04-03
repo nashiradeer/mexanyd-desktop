330565b173bccf4b4961044fd7fb28aaaf59611d
Nashira Deer <nashiradeer@gmail.com>
2024-04-03 20:27:03 -0300

    feat: implement in_out_input logic

--- lib/in_out_input.dart
+++ lib/in_out_input.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mexanyd_desktop/database/interface.dart';
 
 class InOutInput extends StatefulWidget {
-  const InOutInput({super.key});
+  final IDatabase database;
+  const InOutInput(this.database, {super.key});
 
   @override
   State<InOutInput> createState() => _InOutInputState();
 }
 
 class _InOutInputState extends State<InOutInput> {
+  final TextEditingController _valueController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  bool _error = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -24,10 +30,12 @@ class _InOutInputState extends State<InOutInput> {
               margin: const EdgeInsets.only(top: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     width: 90,
                     child: TextField(
+                      controller: _valueController,
                       inputFormatters: [
                         FilteringTextInputFormatter.allow(RegExp(r'[0-9\.,]')),
                         FilteringTextInputFormatter.deny(RegExp(r','),
@@ -39,6 +47,7 @@ class _InOutInputState extends State<InOutInput> {
                       decoration: InputDecoration(
                         labelText: "Valor",
                         counterText: "",
+                        errorText: _error ? "Valor inválido" : null,
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
@@ -50,6 +59,7 @@ class _InOutInputState extends State<InOutInput> {
                       constraints: const BoxConstraints(maxWidth: 600),
                       margin: const EdgeInsets.only(left: 10),
                       child: TextField(
+                        controller: _descriptionController,
                         decoration: InputDecoration(
                           labelText: "Descrição",
                           border: OutlineInputBorder(
@@ -77,7 +87,23 @@ class _InOutInputState extends State<InOutInput> {
                             MaterialStateProperty.all(const Size(0, 60)),
                       ),
                       onPressed: () {
-                        throw UnimplementedError();
+                        try {
+                          final double value =
+                              double.parse(_valueController.text);
+                          final String description =
+                              _descriptionController.text;
+                          widget.database
+                              .insertInOut(value, description: description);
+                          _valueController.clear();
+                          _descriptionController.clear();
+                          setState(() {
+                            _error = false;
+                          });
+                        } catch (e) {
+                          setState(() {
+                            _error = true;
+                          });
+                        }
                       },
                       child: const Text("Entrada",
                           style: TextStyle(color: Colors.white, fontSize: 20)),
@@ -94,7 +120,23 @@ class _InOutInputState extends State<InOutInput> {
                               MaterialStateProperty.all(const Size(0, 60)),
                         ),
                         onPressed: () {
-                          throw UnimplementedError();
+                          try {
+                            final double value =
+                                double.parse(_valueController.text);
+                            final String description =
+                                _descriptionController.text;
+                            widget.database
+                                .insertInOut(-value, description: description);
+                            _valueController.clear();
+                            _descriptionController.clear();
+                            setState(() {
+                              _error = false;
+                            });
+                          } catch (e) {
+                            setState(() {
+                              _error = true;
+                            });
+                          }
                         },
                         child: const Text("Saída",
                             style:
