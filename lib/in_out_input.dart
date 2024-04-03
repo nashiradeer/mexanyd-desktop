a487775fb14fef92fb153d0fb3829c0495556b51
Nashira Deer <nashiradeer@gmail.com>
2024-04-03 19:13:12 -0300

    chore: remove unused import

--- lib/in_out_input.dart
+++ lib/in_out_input.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 class InOutInput extends StatefulWidget {
   const InOutInput({super.key});
