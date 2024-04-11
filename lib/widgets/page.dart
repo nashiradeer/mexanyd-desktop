3902732b09b1cc81d6bbf0b63a4a0bdff395b4fa
nashiradeer <nashiradeer@gmail.com>
2024-04-11 08:16:17 -0300

    chore: cleanup imports

--- lib/widgets/page.dart
+++ lib/widgets/page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
 class MexanydPage extends StatelessWidget {
