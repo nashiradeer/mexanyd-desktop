620a245cce263ad2e58f34b4fa6c96a5f264100b
nashiradeer <nashiradeer@gmail.com>
2024-04-18 08:42:02 -0300

    feat: initialization error page

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ void main() async {
 
   final database = prefs.getString("database") ?? "local";
 
+  String? error;
   if (database == "local") {
     prefs.setString("database", "local");
     final databaseVersion =
@@ -50,18 +51,21 @@ void main() async {
     if (databaseVersion == LocalDatabase.version) {
       prefs.setInt("database_version", LocalDatabase.version);
       globalDatabase = await LocalDatabase.open();
-
-      runApp(MainApp(theme));
     } else {
-      runApp(ErrorApp(theme, "Versão do banco de dados inválida"));
+      error = "Versão do banco de dados inválida";
     }
+  } else {
+    error = "Banco de dados inválido";
   }
+
+  runApp(MainApp(theme, error: error));
 }
 
 class MainApp extends StatelessWidget {
   final ThemeMode theme;
+  final String? error;
 
-  const MainApp(this.theme, {super.key});
+  const MainApp(this.theme, {super.key, this.error});
 
   @override
   Widget build(BuildContext context) {
@@ -69,63 +73,52 @@ class MainApp extends StatelessWidget {
       darkTheme: darkTheme(),
       theme: lightTheme(),
       themeMode: theme,
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/inout/list':
-            return _pageRoute(const InOutListPage());
-          default:
-            return _pageRoute(const InOutInputPage());
-        }
-      },
+      onGenerateRoute: (error == null) ? _pageRoute : null,
+      home: (error == null) ? null : _errorPage(context, error!),
     );
   }
 
-  PageRouteBuilder _pageRoute(Widget page) {
+  PageRouteBuilder _pageRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/inout/list':
+        return _pageRouteBuilder(const InOutListPage());
+      default:
+        return _pageRouteBuilder(const InOutInputPage());
+    }
+  }
+
+  PageRouteBuilder _pageRouteBuilder(Widget page) {
     return PageRouteBuilder(
       pageBuilder: (context, animation1, animation2) => page,
       transitionDuration: Duration.zero,
       reverseTransitionDuration: Duration.zero,
     );
   }
-}
 
-class ErrorApp extends StatelessWidget {
-  final String message;
-  final ThemeMode theme;
-
-  const ErrorApp(this.theme, this.message, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: darkTheme(),
-      theme: lightTheme(),
-      themeMode: theme,
-      home: Material(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.error, size: 128, color: Colors.red),
-              const SizedBox(height: 10),
-              Text(
-                message,
-                style:
-                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  Widget _errorPage(BuildContext context, String message) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.error, size: 128, color: Colors.red),
+            const SizedBox(height: 10),
+            Text(
+              message,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 80),
+            TextButton(
+              onPressed: () => exit(1),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                foregroundColor: MaterialStateProperty.all(
+                    Theme.of(context).colorScheme.onPrimary),
+                fixedSize: MaterialStateProperty.all(const Size(200, 55)),
               ),
-              const SizedBox(height: 80),
-              TextButton(
-                onPressed: () => exit(1),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                  foregroundColor: MaterialStateProperty.all(
-                      Theme.of(context).colorScheme.onPrimary),
-                  fixedSize: MaterialStateProperty.all(const Size(200, 55)),
-                ),
-                child: const Text('Fechar', style: TextStyle(fontSize: 22)),
-              ),
-            ],
-          ),
+              child: const Text('Fechar', style: TextStyle(fontSize: 22)),
+            ),
+          ],
         ),
       ),
     );
