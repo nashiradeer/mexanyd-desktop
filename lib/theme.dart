f8bab73a4a7b67fa227806ed4ef044f021670c87
nashiradeer <nashiradeer@gmail.com>
2024-04-20 08:41:42 -0300

    feat: set PrimaryColor and onPrimary

--- lib/theme.dart
+++ lib/theme.dart
@@ -10,8 +10,10 @@ ThemeData lightTheme() {
   var baseTheme = ThemeData.light(useMaterial3: true);
 
   return baseTheme.copyWith(
+    primaryColor: Colors.blue[700],
     colorScheme: baseTheme.colorScheme.copyWith(
       primary: Colors.blue[700],
+      onPrimary: Colors.white,
     ),
   );
 }
@@ -20,11 +22,13 @@ ThemeData darkTheme() {
   var baseTheme = ThemeData.dark(useMaterial3: true);
 
   return baseTheme.copyWith(
+    primaryColor: Colors.blue,
     colorScheme: baseTheme.colorScheme.copyWith(
       primary: Colors.blue,
       background: Colors.black,
       surface: Colors.transparent,
       surfaceVariant: Colors.grey[900],
+      onPrimary: Colors.white,
       onBackground: Colors.white,
       onSurface: Colors.white,
       onSurfaceVariant: Colors.white,
